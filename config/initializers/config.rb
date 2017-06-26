5eab47233029d6eb156b22f4c53cf55aa4b13321
Chu Thanh Hung <thanhhungchu95@gmail.com>
2017-06-26 22:26:15 +0700

    init-project: Init Framgia Project 2 done

---
+++ config/initializers/config.rb
@@ -0,0 +1,11 @@
+Config.setup do |config|
+  config.const_name = "Settings"
+  config.env_separator = "."
+
+  # config.knockout_prefix = nil
+  # config.overwrite_arrays = true
+  # config.use_env = false
+  # config.env_prefix = "Settings"
+  # config.env_converter = :downcase
+  # config.env_parse_values = true
+end
